edfdc6b491fd579c863555a0aca6b7fc8e73e9d4
alban-okoby <albanokoby225@gmail.com>
2025-12-22 15:18:58 +0000

    React redux

---
+++ 18-react-blog/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+     <div className="App">
+      <CreatePost/>
+      <PostList/>
+    </div>
+  );
+}
+
+export default App;
